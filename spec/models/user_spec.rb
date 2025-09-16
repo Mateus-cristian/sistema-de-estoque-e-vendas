afbab4a3e764d88e641baaa2bc18e03759bfd481
Mateus-cristian <mateus_cristian@outlook.com>
2025-09-15 21:33:04 -0300

    feat: create factory and model tests for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with name, email, and role" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    user = FactoryBot.build(:user, name: nil)
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without an email" do
+    user = FactoryBot.build(:user, email: nil)
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without a role" do
+    user = FactoryBot.build(:user, role: nil)
+    expect(user).to be_invalid
+  end
+
+  it "is invalid with an invalid role" do
+    user = FactoryBot.build(:user, role: "manager")
+    expect(user).to be_invalid
+  end
+
+  it "is invalid with a duplicate email" do
+    FactoryBot.create(:user, email: "test@example.com")
+    user = FactoryBot.build(:user, email: "test@example.com")
+    expect(user).to be_invalid
+  end
+end
