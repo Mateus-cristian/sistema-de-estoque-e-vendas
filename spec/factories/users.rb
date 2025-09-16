8c0fb8be0acb9bbdbead3b11228d34ae909c1cd2
Mateus-cristian <mateus_cristian@outlook.com>
2025-09-15 23:07:28 -0300

    feat: add factory and models tests for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,10 @@
 
 FactoryBot.define do
   factory :user do
-    name  { Faker::Name.name }
-    email { Faker::Internet.email }
-    role  { "user" }
+    name { Faker::Name.name }
+    email { Faker::Internet.unique.email }
+    role { "user" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,4 +33,19 @@ RSpec.describe User, type: :model do
     user = FactoryBot.build(:user, email: "test@example.com")
     expect(user).to be_invalid
   end
+
+  it "is invalid with a short password" do
+    user = FactoryBot.build(:user, password: "123", password_confirmation: "123")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid when password and password_confirmation do match" do
+    user = FactoryBot.build(:user, password: "password", password_confirmation: "password")
+    expect(user).to be_valid
+  end
+
+  it "is invalid when password and password_confirmation do not match" do
+    user = FactoryBot.build(:user, password: "password", password_confirmation: "different")
+    expect(user).not_to be_valid
+  end
 end
