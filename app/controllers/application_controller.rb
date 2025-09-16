09fe45cc8f25e6528570411381cfeae735be70da
Mateus-cristian <mateus_cristian@outlook.com>
2025-09-15 23:14:10 -0300

    feat: add url for mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :role ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :role ])
+  end
 end
