d7ae3e71d97568e5f207e4e441ba69bd2aae38f9
mateus cristian <mateus_cristian@outlook.com>
2025-09-17 20:38:55 -0300

    feat: create product controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,27 @@
+class ProductsController < ApplicationController
+  include AdminAuthorization
+  before_action :authenticate_user!
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Produto criado com sucesso"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :quantity)
+  end
+end
