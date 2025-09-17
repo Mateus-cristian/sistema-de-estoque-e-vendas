50cb83dd2a0d3034d762fcb8e070870cd8297f85
mateus cristian <mateus_cristian@outlook.com>
2025-09-17 11:52:03 -0300

    feat: create product migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_17_130047) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_17_143628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
