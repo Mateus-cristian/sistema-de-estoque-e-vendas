30e2f69cf5126e324ad982e2372b57ff5263d488
mateus cristian <mateus_cristian@outlook.com>
2025-09-18 16:07:58 -0300

    feat: sale controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :sales, only: [:index, :new, :create]
 
   namespace :admin do
     get "dashboard", to: "dashboard#index", as: :dashboard
