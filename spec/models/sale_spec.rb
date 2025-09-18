8e402d4aaabe145b8bcf70c92846f7e3445510b5
mateus cristian <mateus_cristian@outlook.com>
2025-09-18 14:44:13 -0300

    feat: red test for model sale

---
+++ spec/models/sale_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Sale, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:product) { FactoryBot.create(:product, price: 10.0, quantity: 5) }
+
+  it "is valid with valid attributes" do
+    sale = Sale.new(user: user, product: product, quantity: 2)
+    expect(sale).to be_valid
+  end
+
+  it "is invalid without a user" do
+    sale = Sale.new(product: product, quantity: 2)
+    expect(sale).not_to be_valid
+  end
+
+  it "is invalid without a product" do
+    sale = Sale.new(user: user, quantity: 2)
+    expect(sale).not_to be_valid
+  end
+
+  it "is invalid without quantity" do
+    sale = Sale.new(user: user, product: product)
+    expect(sale).not_to be_valid
+  end
+
+  it "is invalid if quantity is greater than product stock" do
+    sale = Sale.new(user: user, product: product, quantity: 10)
+    expect(sale).not_to be_valid
+  end
+
+  it "calculates total correctly" do
+    sale = Sale.new(user: user, product: product, quantity: 3)
+    sale.valid? # triggers calculation
+    expect(sale.total).to eq(30.0)
+  end
+end
