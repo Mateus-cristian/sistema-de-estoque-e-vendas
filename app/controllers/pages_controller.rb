232b4194a7080da01a7e215c9da1aca776dcec4c
mateus cristian <mateus_cristian@outlook.com>
2025-09-19 16:13:18 -0300

    fix: rubocop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,8 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
     redirect_to home_path
-      else
+    else
         redirect_to new_user_session_path
-      end
+    end
   end
 end
