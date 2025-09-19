eb5ae89b30e8913416759ff725e7cd0a5d19233b
mateus cristian <mateus_cristian@outlook.com>
2025-09-19 09:57:27 -0300

    fix: rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,16 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :authenticate_user!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :role ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :role ])
   end
-  
+
   private
-  
+
   def user_not_authorized
     head :forbidden
   end
--- app/policies/sale_policy.rb
+++ app/policies/sale_policy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SalePolicy < ApplicationPolicy
   def show?
     user == record.user
