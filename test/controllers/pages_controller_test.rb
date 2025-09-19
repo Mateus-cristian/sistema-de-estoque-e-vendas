3c4522651546ada1f78c42045231c339c5ff5ca0
mateus cristian <mateus_cristian@outlook.com>
2025-09-19 16:17:58 -0300

    fix: test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -10,6 +10,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
     get root_url
-    assert_response :success
+    assert_response :redirect
   end
 end
