46e059d20621524bcc0936a4b235672593e14887
mateus cristian <mateus_cristian@outlook.com>
2025-09-19 10:18:18 -0300

    fix: test page controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,8 +3,13 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(email: "test@example.com", password: "password", name: "Test")
+    post user_session_path, params: { user: { email: @user.email, password: "password" } }
+  end
+
   test "should get home" do
-    get pages_home_url
+    get root_url
     assert_response :success
   end
 end
