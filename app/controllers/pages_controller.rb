38bfffe487dd06d2d8a5282debff08f01bacfa80
mateus cristian <mateus_cristian@outlook.com>
2025-09-19 16:15:02 -0300

    fix: spaces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,9 @@
 class PagesController < ApplicationController
   def home
     if user_signed_in?
-    redirect_to home_path
+      redirect_to home_path
     else
-        redirect_to new_user_session_path
+      redirect_to new_user_session_path
     end
   end
 end
