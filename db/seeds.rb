f39bfe0a3e72e295e0d0d34f97db988aa1493369
mateus cristian <mateus_cristian@outlook.com>
2025-09-22 07:21:04 -0300

    fix: format seeds.rb for consistent indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-	User.find_or_create_by!(email: 'user.admin@uorak.com') do |user|
-		user.name = 'admin'
-		user.password = '@123password'
-		user.password_confirmation = '@123password'
-		user.role = :admin
-	end
-	puts 'Usuário admin criado: user.admin@uorak.com / @123password'
+  User.find_or_create_by!(email: 'user.admin@uorak.com') do |user|
+    user.name = 'admin'
+    user.password = '@123password'
+    user.password_confirmation = '@123password'
+    user.role = :admin
+  end
+  puts 'Usuário admin criado: user.admin@uorak.com / @123password'
 end
