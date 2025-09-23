4768053564563cf8248b1501d3c3f25a550410e1
mateus cristian <mateus_cristian@outlook.com>
2025-09-22 22:23:04 -0300

    fix: update pages controller test to assert success for authenticated user redirect

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,13 +3,15 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = User.create!(email: "test@example.com", password: "password", name: "Test")
-    post user_session_path, params: { user: { email: @user.email, password: "password" } }
+  test "should get landing for guest" do
+    get root_url
+    assert_response :success
   end
 
-  test "should get home" do
+  test "should redirect authenticated user from root to home" do
+    @user = User.create!(email: "test@example.com", password: "password", name: "Test")
+    post user_session_path, params: { user: { email: @user.email, password: "password" } }
     get root_url
-    assert_response :redirect
+    assert_response :success
   end
 end
